01601668145fc8b5750c84e36fbff572d81b731a
miiiso1983 <mustafaalrawan@gmail.com>
2025-10-05 16:13:30 +0300

    fix(i18n): add missing translations for common.* used by interactive-chart to prevent array_replace_recursive int error; provide en/ar

--- lang/en/common.php
+++ lang/en/common.php
@@ -1,1 +1,8 @@
+<?php
 
+return [
+    'loading_chart' => 'Loading chart...',
+    'no_chart_data' => 'No chart data available',
+    'download_chart' => 'Download chart',
+    'refresh_chart' => 'Refresh chart',
+];
